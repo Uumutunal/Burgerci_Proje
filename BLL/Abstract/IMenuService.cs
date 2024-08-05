ff2e87c81e5e4e0d430bb30a6f0cf6c4a7180074
Uumutunal <unal.umut98@gmail.com>
2024-08-05 17:07:36 +0300

    menu order

---
+++ BLL/Abstract/IMenuService.cs
@@ -0,0 +1,16 @@
+﻿using BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Abstract
+{
+    public interface IMenuService
+    {
+        Task<List<MenuDto>> GetAllMenus();
+        Task CreateMenu(MenuDto menuDto);
+        Task DeleteMenu(Guid menuId);
+    }
+}
---
+++ BLL/Concrete/MenuService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using BLL.Abstract;
+using BLL.DTOs;
+using Burgerci_Proje.Entities;
+using DAL.AbstractRepositories;
+using DAL.Entities;
+
+namespace BLL.Concrete
+{
+    public class MenuService : IMenuService
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepository<Menu> _menuRepository;
+        public MenuService(IRepository<Menu> menuRepository, IMapper mapper)
+        {
+            _menuRepository = menuRepository;
+            _mapper = mapper;
+        }
+        public async Task CreateMenu(MenuDto menuDto)
+        {
+            var menu = _mapper.Map<Menu>(menuDto);
+            await _menuRepository.AddAsync(menu);
+        }
+
+        public async Task DeleteMenu(Guid menuId)
+        {
+            await _menuRepository.DeleteAsync(menuId);
+        }
+
+        public async Task<List<MenuDto>> GetAllMenus()
+        {
+            var menus = await _menuRepository.GetAllAsync();
+            var garnitureDtos = _mapper.Map<List<MenuDto>>(menus);
+            return garnitureDtos;
+        }
+    }
+}
