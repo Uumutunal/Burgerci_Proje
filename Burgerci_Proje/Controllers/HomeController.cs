9465bc03075fa68ed0e9ed73c84d376fe1c75181
Kaan Aydın <muhammedkagan.aydin@bilgeadamboost.com>
2024-08-09 11:16:19 +0300

    anasayfa

--- Burgerci_Proje/Controllers/HomeController.cs
+++ Burgerci_Proje/Controllers/HomeController.cs
@@ -1,12 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using Burgerci_Proje.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Burgerci_Proje.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IDrinkService _drinkService;
+        private readonly IExtraService _extraService;
+        private readonly IHamburgerService _hamburgerService;
+
+        public HomeController(IDrinkService drinkService, IExtraService extraService, IHamburgerService hamburgerService)
         {
-            return View();
+            _drinkService = drinkService;
+            _extraService = extraService;
+            _hamburgerService = hamburgerService;
         }
+        public async Task<IActionResult> Index(string category = "All")
+        {
+            var model = new MenuItemsViewModel
+            {
+                Drinks = (await _drinkService.GetAllDrinks()).Select(d => new DrinkViewModel
+                {
+                    Name = d.Name,
+                    Price = d.Price,
+                    Quantity = d.Quantity,
+                    Size = d.Size,
+                    Photo = d.ImageUrl
+                }).ToList(),
+                Extras = (await _extraService.GetAllExtra()).Select(e => new ExtraViewModel
+                {
+                    Name = e.Name,
+                    Description = e.Description,
+                    Price = e.Price,
+                    Quantity = e.Quantity,
+                    Size = e.Size,
+                    Photo = e.ImageUrl
+                }).ToList(),
+                Hamburgers = (await _hamburgerService.GetAllHamburgers()).Select(h => new HamburgerViewModel
+                {
+                    Name = h.Name,
+                    Description = h.Description,
+                    Price = h.Price,
+                    SelectedGarnitureIds = h.SelectedGarnitureIds,
+                    Quantity = h.Quantity,
+                    Size = h.Size,
+                    Photo = h.ImageUrl
+                }).ToList(),
+
+                SelectedCategory = category
+            };
+
+            switch (category)
+            {
+                case "Hamburger":
+                    model.Drinks = new List<DrinkViewModel>();
+                    model.Extras = new List<ExtraViewModel>();
+                    break;
+                case "Drink":
+                    model.Hamburgers = new List<HamburgerViewModel>();
+                    model.Extras = new List<ExtraViewModel>();
+                    break;
+                case "Extra":
+                    model.Hamburgers = new List<HamburgerViewModel>();
+                    model.Drinks = new List<DrinkViewModel>();
+                    break;
+                case "All":
+                default:
+                    break;
+            }
+
+            return PartialView(model);
+        }
+
+
     }
 }
---
+++ Burgerci_Proje/Models/MenuItemsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Burgerci_Proje.Models
+{
+    public class MenuItemsViewModel
+    {
+        public List<DrinkViewModel> Drinks { get; set; }
+        public List<ExtraViewModel> Extras { get; set; }
+        public List<HamburgerViewModel> Hamburgers { get; set; }
+        public string SelectedCategory { get; set; }
+    }
+}
